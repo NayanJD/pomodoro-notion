692831bb76b97f93b076eb994ccedf3500cf5255
Nayan Das <ndas@digitalocean.com>
2025-03-21 12:36:19 +0530

    Complete go server

--- server_v2/go.mod
+++ server_v2/go.mod
@@ -1,3 +1,5 @@
 module pomodoro_notion
 
 go 1.23.2
+
+require github.com/rs/cors v1.11.1
--- server_v2/server.go
+++ server_v2/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"github.com/rs/cors"
 	"log/slog"
 	"net"
 	"net/http"
@@ -13,7 +16,13 @@ import (
 
 type Response struct {
 	Error  string      `json:"error,omitempty"`
-	Result interface{} `json:"result,omitempty"`
+	Result interface{} `json:"results,omitempty"`
+}
+
+type SessionRequest struct {
+	TaskId    string `json:"taskId"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
 }
 
 type NotionPage struct {
@@ -22,18 +31,20 @@ type NotionPage struct {
 }
 
 type Config struct {
-	ProjectTasksID string
-	NotionAPIKey   string
-	Port           string
+	ProjectTasksID      string
+	PomodoroSessionDbId string
+	NotionAPIKey        string
+	Port                string
 }
 
 func loadConfig() (*Config, error) {
 	projectTasksID := os.Getenv("PROJECT_TASKS_DB_ID")
 	notionAPIKey := os.Getenv("NOTION_API_KEY")
+	pomodoroSessionDbId := os.Getenv("POMODORO_SESSIONS_DB_ID")
 	port := os.Getenv("PORT")
 
-	if projectTasksID == "" || notionAPIKey == "" {
-		return nil, fmt.Errorf("PROJECT_TASKS_DB_ID and NOTION_API_KEY must be set")
+	if projectTasksID == "" || notionAPIKey == "" || pomodoroSessionDbId == "" {
+		return nil, fmt.Errorf("PROJECT_TASKS_DB_ID, NOTION_API_KEY and POMODORO_SESSIONS_DB_ID must be set")
 	}
 
 	if port == "" {
@@ -41,15 +52,26 @@ func loadConfig() (*Config, error) {
 	}
 
 	return &Config{
-		ProjectTasksID: projectTasksID,
-		NotionAPIKey:   notionAPIKey,
-		Port:           port,
+		ProjectTasksID:      projectTasksID,
+		NotionAPIKey:        notionAPIKey,
+		PomodoroSessionDbId: pomodoroSessionDbId,
+		Port:                port,
 	}, nil
 }
 
 func handleTasks(cfg *Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		logger := slog.Default().With("handler", "tasks")
+
+		logger.Info("received request")
+		defer logger.Info("handled request")
+
 		w.Header().Set("Content-Type", "application/json")
+
+		// if r.Method == http.MethodOptions {
+		// 	setupCorsResponse(w, r)
+		// 	return
+		// }
 
 		if r.Method != http.MethodGet {
 			sendJSONResponse(w, http.StatusMethodNotAllowed, Response{
@@ -63,9 +85,17 @@ func handleTasks(cfg *Config) http.HandlerFunc {
 		req, err := http.NewRequestWithContext(ctx,
 			"POST",
 			fmt.Sprintf("https://api.notion.com/v1/databases/%s/query", cfg.ProjectTasksID),
-			nil)
+			bytes.NewBuffer([]byte(`{
+        "filter": {
+          "property": "Status",
+          "status": {
+            "equals": "In Progress"
+          }
+        }
+      }`)))
+
 		if err != nil {
-			slog.Error("Error constructing the request", "handler", "tasks", "err", err)
+			logger.Error("Error constructing the request", "err", err)
 			sendJSONResponse(w, http.StatusInternalServerError, Response{
 				Error: "failed to construct request",
 			})
@@ -80,7 +110,7 @@ func handleTasks(cfg *Config) http.HandlerFunc {
 		client := &http.Client{Timeout: 10 * time.Second}
 		resp, err := client.Do(req)
 		if err != nil || (resp != nil && resp.StatusCode >= 500 && resp.StatusCode < 600) {
-			slog.Error("Error while making request to notion", "handler", "tasks", "err", err, "statusCode", resp.StatusCode)
+			logger.Error("Error while making request to notion", "err", err, "statusCode", resp.StatusCode)
 			sendJSONResponse(w, http.StatusInternalServerError, Response{
 				Error: "failed to fetch tasks from Notion",
 			})
@@ -92,7 +122,7 @@ func handleTasks(cfg *Config) http.HandlerFunc {
 			var anyErr interface{}
 			json.NewDecoder(resp.Body).Decode(&anyErr)
 
-			slog.Error("Non-200 response from Notion", "handler", "tasks", "status", resp.StatusCode, "error", anyErr)
+			logger.Error("Non-200 response from Notion", "status", resp.StatusCode, "error", anyErr)
 			sendJSONResponse(w, http.StatusInternalServerError, Response{
 				Error: "received error response from Notion",
 			})
@@ -101,7 +131,132 @@ func handleTasks(cfg *Config) http.HandlerFunc {
 
 		var notionResp NotionPage
 		if err := json.NewDecoder(resp.Body).Decode(&notionResp); err != nil {
-			slog.Error("Error while decoding json body", "handler", "tasks", "err", err)
+			logger.Error("Error while decoding json body", "err", err)
+			sendJSONResponse(w, http.StatusInternalServerError, Response{
+				Error: "failed to parse Notion response",
+			})
+			return
+		}
+
+		sendJSONResponse(w, http.StatusOK, Response{
+			Result: notionResp.Results,
+		})
+	}
+}
+
+func handleSession(cfg *Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := slog.Default().With("handler", "log-session")
+
+		w.Header().Set("Content-Type", "application/json")
+
+		if r.Method == http.MethodOptions {
+			setupCorsResponse(w, r)
+			return
+		}
+
+		if r.Method != http.MethodPost {
+			sendJSONResponse(w, http.StatusMethodNotAllowed, Response{
+				Error: "method not allowed",
+			})
+			return
+		}
+
+		sessionReqBody := SessionRequest{}
+
+		err := json.NewDecoder(r.Body).Decode(&sessionReqBody)
+		if err != nil {
+			logger.Error("Error while decoding request body", "err", err)
+			sendJSONResponse(w, http.StatusBadRequest, Response{
+				Error: "failed to parse request body",
+			})
+			return
+		}
+
+		taskId := sessionReqBody.TaskId
+
+		startTime, err := time.Parse(time.RFC3339, sessionReqBody.StartTime)
+		if err != nil {
+			logger.Error("Error while parsing start time date", "err", err)
+			sendJSONResponse(w, http.StatusBadRequest, Response{
+				Error: "failed to parse start time",
+			})
+			return
+		}
+
+		endTime, err := time.Parse(time.RFC3339, sessionReqBody.EndTime)
+		if err != nil {
+			logger.Error("Error while parsing start time date", "err", err)
+			sendJSONResponse(w, http.StatusBadRequest, Response{
+				Error: "failed to parse end time",
+			})
+			return
+		}
+
+		sessionBody := fmt.Sprintf(`{
+        "parent": { "database_id": "%v" },
+        "properties": {
+          "Name": {
+            "type": "title",
+            "title": [
+              {
+                "text": {
+                  "content": "Session-%v"
+                }
+              }
+            ]
+          },
+          "Start Date": { "type": "date", "date": { "start": "%v" } },
+          "End Date": { "type": "date", "date": { "start": "%v" } },
+          "Project Tasks": { "type": "relation", "relation": [{ "id": "%v" }] }
+        }
+      }`, cfg.PomodoroSessionDbId, generateRandomString(6), startTime.Format(time.RFC3339), endTime.Format(time.RFC3339), taskId)
+
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+
+		req, err := http.NewRequestWithContext(ctx,
+			"POST",
+			"https://api.notion.com/v1/pages",
+			bytes.NewBuffer([]byte(sessionBody)))
+		if err != nil {
+			logger.Error("Error constructing the request", "err", err)
+			sendJSONResponse(w, http.StatusInternalServerError, Response{
+				Error: "failed to construct request",
+			})
+
+			return
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", cfg.NotionAPIKey))
+		req.Header.Add("Notion-Version", "2022-06-28")
+		req.Header.Add("Content-Type", "application/json")
+
+		client := &http.Client{Timeout: 10 * time.Second}
+		resp, err := client.Do(req)
+		if err != nil || (resp != nil && resp.StatusCode >= 500 && resp.StatusCode < 600) {
+			logger.Error("Error while making request to notion", "err", err, "statusCode", resp.StatusCode)
+			sendJSONResponse(w, http.StatusInternalServerError, Response{
+				Error: "failed to fetch tasks from Notion",
+			})
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			var anyErr interface{}
+			json.NewDecoder(resp.Body).Decode(&anyErr)
+
+			logger.Error("Non-200 response from Notion", "status", resp.StatusCode, "error", anyErr)
+			sendJSONResponse(w, http.StatusInternalServerError, Response{
+				Error: "received error response from Notion",
+			})
+			return
+		}
+
+		var notionResp NotionPage
+		if err := json.NewDecoder(resp.Body).Decode(&notionResp); err != nil {
+			logger.Error("Error while decoding json body", "err", err)
 			sendJSONResponse(w, http.StatusInternalServerError, Response{
 				Error: "failed to parse Notion response",
 			})
@@ -121,6 +276,12 @@ func sendJSONResponse(w http.ResponseWriter, status int, payload interface{}) {
 	}
 }
 
+func setupCorsResponse(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func main() {
 	cfg, err := loadConfig()
 	if err != nil {
@@ -130,10 +291,19 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/tasks", handleTasks(cfg))
+	mux.HandleFunc("/log-session", handleSession(cfg))
+
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{http.MethodPost, http.MethodGet},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+	})
 
 	httpServer := &http.Server{
-		Addr:         net.JoinHostPort("0.0.0.0", cfg.Port),
-		Handler:      mux,
+		Addr: net.JoinHostPort("0.0.0.0", cfg.Port),
+		Handler:      cors.Handler(mux),
+		// Handler:      mux,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 		IdleTimeout:  60 * time.Second,
@@ -144,4 +314,19 @@ func main() {
 		slog.Error("Server failed", "err", err)
 		os.Exit(1)
 	}
+}
+
+func generateRandomString(length uint32) string {
+
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	b := make([]byte, length)
+
+	rand.Read(b)
+
+	for i, _ := range b {
+		b[i] = charset[int(b[i])%len(charset)]
+	}
+
+	return string(b)
 }
